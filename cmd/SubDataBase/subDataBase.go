9878435429188025b0f843601ee255190e2b1d99
AntonMestre <anmaystre@gmail.com>
2021-12-23 16:20:02 +0100

    ajout de la bd

--- cmd/SubDataBase/subDataBase.go
+++ cmd/SubDataBase/subDataBase.go
@@ -14,40 +14,47 @@ import (
 )
 
 func main() {
-	initDb()
 
+	// Connection to database (MongoDb Cloud)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	dbClient, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://Airport:Airport@cluster0.0c6je.mongodb.net/AirportDataBase?retryWrites=true&w=majority"))
+	defer func() {
+		if err = dbClient.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Callback function called when a message is recieved from the broker
+	var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+		fmt.Printf("Reception d'un message  - %s\n", msg.Payload())
+
+		var collection *mongo.Collection
+
+		if msg.Topic() == util.TOPIC_WIND {
+			collection = dbClient.Database("AirportDataBase").Collection("Wind")
+		} else if msg.Topic() == util.TOPIC_TEMP {
+			collection = dbClient.Database("AirportDataBase").Collection("Temp")
+		} else {
+			collection = dbClient.Database("AirportDataBase").Collection("Pressure")
+		}
+
+		res, err := collection.InsertOne(ctx, bson.D{{"idCaptor", 1}, {"iATA", "TLS"}, {"value", 40}, {"pickingDate", time.Now()}})
+		fmt.Printf("res  - %s\n", res)
+		fmt.Printf("err  - %s\n", err)
+	}
+
+	// Connecting to the broker in subscriber mode
 	client := tools.Connect(util.HOST, util.CLIENT_DATABASE_SUB)
 	tokenWind := client.Subscribe(util.TOPIC_WIND, 2, messagePubHandler)
 	tokenTemp := client.Subscribe(util.TOPIC_TEMP, 2, messagePubHandler)
 	tokenPressure := client.Subscribe(util.TOPIC_PRESSURE, 2, messagePubHandler)
 
+	// Waiting for message from broker
 	for {
 		tokenWind.Wait()
 		tokenTemp.Wait()
 		tokenPressure.Wait()
 	}
 
-}
-
-func initDb() {
-
-}
-
-var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	dbClient, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://Airport:Airport@cluster0.0c6je.mongodb.net/AirportDataBase?retryWrites=true&w=majority"))
-	fmt.Sprintf("%d", err)
-	defer func() {
-		if err = dbClient.Disconnect(ctx); err != nil {
-			panic(err)
-		}
-	}()
-	fmt.Printf("Ajout en BD de  - %s\n", msg.Payload())
-	collection := dbClient.Database("AirportDataBase").Collection("Pressure")
-	res, err := collection.InsertOne(ctx, bson.D{{"idCaptor", 1}, {"iATA", "TLS"}, {"value", 40}, {"pickingDate", time.Now()}})
-	fmt.Printf("res  - %s\n", res)
-	fmt.Printf("err  - %s\n", err)
-	fmt.Sprintf("%d", res)
-	fmt.Sprintf("%d", err)
 }
